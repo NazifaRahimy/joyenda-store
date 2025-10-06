ba9d0b7b0ae2a74d137e247ca0a7c467b0c8d640
nazifaRahimi <nazifarahimy6@gmail.com>
2025-10-06 08:47:23 +0430

    add destopContact , MobileContact components for Contact

--- src/app/Contact/page.tsx
+++ src/app/Contact/page.tsx
@@ -1,10 +1,11 @@
+import MobileContact from "@/components/MobileContact";
+import DesktopContact from "@/components/DesktopContact";
 const Contact = () => {
     return ( 
         <main className="p-8">
-      <h1 className="text-3xl font-bold">Contact</h1>
-      <p className=" bg-lime-600 p-5 text-lg text-white">Our Contact details go here.</p>this isi a booj a  catthis isi a booj a  cattthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  cathis isi a booj a  catthis isi a booj a  catthis isi a booj a  cat
-      <p>this isi a booj a  cat this isi a booj a  cat this isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  catthis isi a booj a  cat </p>
-    </main> 
+            <DesktopContact/>
+          <MobileContact />
+        </main> 
      );
 }
  
