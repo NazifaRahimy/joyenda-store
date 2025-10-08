4ae0f2c993d260304064fd31b9f27364908af52b
nazifaRahimi <nazifarahimy6@gmail.com>
2025-10-08 11:28:09 +0430

    Add login form with validation and API request

--- src/components/LoginMobile.tsx
+++ src/components/LoginMobile.tsx
@@ -2,18 +2,69 @@
 import Link from "next/link";
 import { IoEye  } from "react-icons/io5";
 import { PiEyeClosedBold } from "react-icons/pi";
+import { useRouter } from "next/navigation";
 import { useState } from "react";
 import Image from "next/image";
 
     const LoginMobile = () => {
         const [showPassword, setShowPassword] = useState(false);
-        const [formDate, setformData] = useState({email: "",  password: ""})
- 
+        const [formDate, setformDate] = useState({email: "",  password: ""})
+        const [formErrors, setFormErrors] = useState<Record<string, string>>({ email: "", password: ""});
+        const router = useRouter();
+        const [agree, setAgree]= useState(false)
+
         let names , values
         const  data =(e: React.ChangeEvent<HTMLInputElement>) => {
             names = e.target.name;
 	        values = e.target.value;
-            setformData({...formDate, [names]: values})
+            setformDate({...formDate, [names]: values})
+        }
+
+        const isValidEmail = (email: string) => {
+            return /^[^\s@]+@[^\s@]+\.[^\s@]+$/.test(email);
+        };
+    
+
+        const handleLogin = async (e: React.FormEvent<HTMLFormElement>)=>{
+            e.preventDefault();
+
+            const errors: Record<string, string> = {};
+            if(!formDate.email){
+                errors.email= "Email must be a valid email address."
+            }
+        
+            if(!formDate.password){
+                errors.password =  "Password must be at least 6 characters long."
+            }
+            setFormErrors(errors)
+            if (Object.keys(errors).length > 0) return;
+
+            try {
+                const response = await fetch("http://localhost:4000/lagin",{
+                    method: "POST",
+                    headers: { "Content-Type": "application/json" },
+                    body: JSON.stringify({
+                        email: formDate.email,
+                        password: formDate.password
+                    })
+                })
+                const data = await response.json();
+                if(response.ok){
+                    const token = "test-token-123"; // توکن تستی
+                    alert("Login successful");
+                    localStorage.setItem("auth-token", token);
+                    localStorage.setItem("auth-name", data.first_name );
+                    localStorage.setItem("auth-email", data.email);
+                    setformDate({email:"", password:""});
+                    router.replace("/")
+                } else {
+                    alert(data.message || "Login failed");
+                }
+
+            }catch(error){
+                alert("Network error. Please check your connection.");
+            }
+
         }
         
         
@@ -25,21 +76,23 @@ import Image from "next/image";
                 <div className="w-[120px] h-[120px] rounded-full  border-4 border-white dark:border-[#101010] ring-4 ring-violet-600 bg-violet-600 text-white text-3xl flex items-center justify-center"> جوینده</div>
                 <p className="  mt-2 text-xl text-violet-600 tracking-wide">AFghanistan Online Market </p>
             </div>
-            <form  className="flex flex-col ">
+            <form onSubmit={handleLogin}  className="flex flex-col ">
                 <input type="text" value={formDate.email} name="email" onChange={data} placeholder="someone@exampe.com"  className="w-full mb-4 px-6 bg-white py-3 border border-gray-400 shadow rounded-full focus:ring-1 focus:ring-violet-500 outline-none dark:bg-[#232323] "/>
+                 {(formErrors.email || formDate.email)&& !isValidEmail(formDate.email)  && ( <p className="text-red-500 text-xs mt-1 ml-4">   {formErrors.email || "Email must be a valid email address."}  </p>  )}
                 <div className="w-full relative">
                     <input  type={showPassword ? "text" : "password"} value={formDate.password} name="password" onChange={data} placeholder="*******"  className="w-full px-6 py-3  shadow rounded-full bg-white border border-gray-400 focus:ring-1 focus:ring-violet-500 outline-none dark:bg-[#232323] "/>
                     <span className="absolute right-6 top-4 cursor-pointer"   onClick={() => setShowPassword(prev => !prev)} >   {showPassword ? <IoEye /> : <PiEyeClosedBold/>}</span>
+                    {(formErrors.password || formDate.password) && formDate.password.length <8 && (<p className="text-red-500 text-xs mt-1 ml-4">{formErrors.password || "Password must be at least 6 characters long."}</p>)}
                 </div>
-                <button type="button" className=" my-6  text-blue-400  border-b border-b-transparent hover:border-b hover:border-red-500 text-[18px] outline-none tracking-wide text-left">	Forget Password ?	</button> 
-                <button  type="submit" className={`bg-violet-600 transition duration-300 hover:bg-violet-500 text-white px-4 py-3 rounded-full text-xl font-semibold w-full flex  items-center justify-center  	`}><span> Login</span></button>
+                <button type="button"  className=" my-6  text-blue-400   hover:border-b hover:border-red-500 text-[18px] outline-none tracking-wide text-left">	Forget Password ?	</button> 
+                <button   type="submit"  disabled={!agree} className={`bg-violet-600 transition duration-300 hover:bg-violet-500 text-white px-4 py-3 rounded-full text-xl font-semibold w-full flex  items-center justify-center  	 ${!agree ? "opacity-40 cursor-not-allowed" : ""}	 `}><span> Login</span></button>
                 <div className=" flex items-center px-4 my-5">
 			        <span className="px-4 text-[16px] tracking-wide">Don not have an account ?		</span> 
                     <span  className=" text-violet-600  text-[16px] tracking-wide"> Registration	</span>
 		 	    </div>
-                <button  className=" px-8 py-2 rounded-full border-2 border-black w-full flex items-center justify-between text-xl   tracking-wide" >Sign with Google <Image src="/google.png" alt="google" width={40} height={40} /></button>
+                <button  type="button"  className=" px-8 py-2 rounded-full border-2 border-black w-full flex items-center justify-between text-xl   tracking-wide" >Sign with Google <Image src="/google.png" alt="google" width={40} height={40} /></button>
                 <div className="flex items-center gap-2 my-5">
-                    <input className="w-5 h-5 border-2 border-black rounded" type="checkbox"  id="agree"  />
+                    <input className={`w-5 h-5 border-2 border-black rounded ${agree ? "bg-violet-500": ""}`} type="checkbox" checked={agree}   id="agree" onChange={(e)=> setAgree(e.target.checked)} />
                     <label htmlFor="agree" className="text-md tracking-wide"> I agree to the Privacy Policy <Link href="/Privacy" className="text-violet-500 underline-none tracking-wide underline">Privacy Policy</Link></label>
                 </div>
             </form>
